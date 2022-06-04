2587028acebfad35b8896f401c547f07c7d47983
WardaBoussayed <boussayedwarda@gmail.com>
2022-06-04 15:48:26 +0200

    CHANGE PORT 8080

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const cors = require("cors");
+const Axios = require("axios");
+const app = express();
+const PORT = 8080;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(cors());
+app.use(express.json());
+
+app.post("/compile", (req, res) => {
+    let code = req.body.code;
+    let language = req.body.language;
+    let input = req.body.input;
+
+
+    if (language === "python") {
+        language="py"
+    }
+
+    let data = ({
+        "code": code,
+        "language": language,
+        "input": input
+
+    });
+    let config = {
+        method: 'post',
+        url: 'https://servercodecompiler.herokuapp.com',
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        data: data
+    };
+    //appel au code de compile API du coup
+    Axios(config)
+        .then((response)=>{
+            res.send(response.data)
+            console.log(response.data)
+        }).catch((error)=>{
+        console.log(error);
+    });
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
